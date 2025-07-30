d4046400456090fc79d73b0e3f07ce55d558aed2
Juan_Tobon <jctobon11.2@gmail.com>
2025-07-29 21:41:59 -0500

    first_commit

---
+++ tenant-api/src/main/java/multi_tenant_back/tenant_api/TenantApiApplication.java
@@ -0,0 +1,13 @@
+package multi_tenant_back.tenant_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantApiApplication.class, args);
+	}
+
+}
---
+++ tenant-api/src/test/java/multi_tenant_back/tenant_api/TenantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package multi_tenant_back.tenant_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
